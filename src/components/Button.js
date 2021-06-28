76a6acfa3ab204b73cd4babb7ffbe0fb06b6575a
Paul Ccari <paulclindo@gmail.com>
2021-06-28 00:53:11 -0500

    create button and typography components

---
+++ src/components/Button.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  border: none;
+  padding-top: 16px;
+  padding-bottom: 16px;
+  min-width: 141px;
+  font-size: 16px;
+  font-weight: bold;
+  cursor: pointer;
+  transition: background-color 0.4s;
+  ${({ variant, theme }) =>
+    variant === "primary" &&
+    `
+    background: ${theme.palette.primary[700]};
+    color: ${theme.palette.white};
+    :hover {
+      background: ${theme.palette.primary[500]};
+      color: ${theme.palette.white};
+    }
+  `}
+  ${({ variant, theme }) =>
+    variant === "secondary" &&
+    `
+    color: ${theme.palette.primary[700]};
+    background-color: ${theme.palette.secondary[300]};
+    :hover {
+      background: ${theme.palette.primary[300]};
+    }
+  `}
+`;
+
+export default function Button({ variant = "primary", ...props }) {
+  return <StyledButton variant={variant} {...props} />;
+}
---
+++ src/components/Typography.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+const mapTypography = {
+  h1: "h1",
+  h2: "h2",
+  h3: "h3",
+  h4: "h4",
+  body: "p",
+};
+const StyledTypography = styled.h1`
+  margin: 0;
+  font-size: ${(props) => props.theme.typography[props.variant].fontSize};
+  line-height: ${(props) => props.theme.typography[props.variant].lineHeight};
+  font-weight: ${(props) => props.theme.typography[props.variant].fontWeight};
+`;
+
+export default function Typography({ variant, ...props }) {
+  const Component = mapTypography[variant] ?? "span";
+  return <StyledTypography as={Component} variant={variant} {...props} />;
+}
